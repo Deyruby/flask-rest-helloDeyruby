192b1a0ed166969fa3c2e1e5f3c5bec0dffe82b8
Deyruby <128061593+Deyruby@users.noreply.github.com>
2024-01-20 17:41:42 +0000

    modificaciones

--- src/app.py
+++ src/app.py
@@ -8,17 +8,17 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Character, Favoritecharacter, CharacterEpisodeAndLocation
 #from models import Person
 
 app = Flask(__name__)
 app.url_map.strict_slashes = False
 
-db_url = os.getenv("DATABASE_URL")
-if db_url is not None:
-    app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
-else:
-    app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
+#db_url = os.getenv("DATABASE_URL")
+#if db_url is not None:
+  #  app.config['SQLALCHEMY_DATABASE_URI'] = db_url.replace("postgres://", "postgresql://")
+#else:
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:////tmp/test.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 MIGRATE = Migrate(app, db)
@@ -31,21 +31,188 @@ setup_admin(app)
 def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
-# generate sitemap with all your endpoints
-@app.route('/')
+@app.route('/') 
 def sitemap():
-    return generate_sitemap(app)
+    return generate_sitemap(app)  
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+#Creamos un usuario
+@app.route('/createuser', methods=["POST"])
+def create():
+  get_from_body = request.json.get("email")
+  user = User() 
+  usuario_existente = User.query.filter_by(email=get_from_body).first()
+  if usuario_existente is not None:
+    return "The User already exist"
+  else:
+    user.name = request.json.get("name")
+    user.last_name = request.json.get("lastname")
+    user.email = request.json.get("email")
+    user.password = request.json.get("password")
+    user.subscription_date =request.json.get("subscription_date")
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    db.session.add(user)
+    db.session.commit()  
 
-    return jsonify(response_body), 200
+    return f"Se creo el usuario", 201
+ 
+ #Actualizo un usuario
+@app.route('/updateuser', methods=["PUT"])
+def update():
+  email_to_search = request.json.get("email")
+  usuario = User.query.filter_by(email=email_to_search).first()
+  if usuario is None:
+    return "The user does not exist", 401
+  else:
+    usuario.name = request.json.get("name")
+    usuario.last_name = request.json.get("lastname")
+    usuario.email = request.json.get("email")
+    usuario.password = request.json.get("password")
+  
+    db.session.add(usuario)
+    db.session.commit()
+
+    return f"Se actualizo el usuario", 201
+ 
+ #Eliminar un usuario
+@app.route("/deleteuser/<int:id>", methods=['DELETE'])
+def delete_user(id):
+  usuario = User.query.filter_by(id=id).first()
+  print(usuario)
+  if usuario is not None:
+    db.session.delete(usuario)
+    db.session.commit()
+    return jsonify({
+      "msg": "Usuario eliminado",
+      "status": "Success"
+    }), 203
+  else:
+    return jsonify({"error":"Usuario no encontrado"}),404
+
+ 
+# Listo todos los usuarios
+@app.route('/users', methods=["GET"])
+def home():
+    users= User.query.all()
+    users= list(map(lambda user: user.serialize_1(), users))
+   
+    return jsonify({
+    "data": users,
+    "status": 'success'
+  }),200
+
+
+  #Agregar Personaje
+@app.route('/createcharacter', methods=["POST"])
+def createcharacter():
+ add_a_character = request.json.get("name")
+ character = Character() 
+ personaje_existente = Character.query.filter_by(name=add_a_character).first()
+ if personaje_existente is not None:
+    return "The character already exist"
+ else:
+    character.name = request.json.get("name")
+    character.status = request.json.get("status")
+    character.species= request.json.get("species")
+    character.gender =request.json.get("gender")
+
+    db.session.add(character)
+    db.session.commit()  
+
+    return f"Se creo el personaje", 201
+
+#Muestro los personajes por su id
+@app.route('/character/<int:id>', methods=["GET"])
+def obtainig_character(id):
+    character: Character.query.filter_by(id=id).first()
+    if character is not None:
+         return jsonify(character.serialize_2()), 200
+    else:
+         return jsonify({"error":"Character not found"}),404
+    
+    
+#Listo los personajes
+@app.route('/character', methods=["GET"])
+def character():
+    characters: Character.query.all()
+    characters: list(map(lambda character: character.serialize_2(), characters))
+   
+    return jsonify({
+    "data": characters,
+    "status": 'success'
+  }),200   
+
+#Elimino personaje  
+@app.route("/deletecharacter/<int:id>", methods=['DELETE'])
+def delete_character(id):
+  character_delete = Character.query.filter_by(id=id).first()
+  if character_delete is not None:
+    db.session.delete(character_delete)
+    db.session.commit()
+    return jsonify({
+      "msg": "Personaje eliminado",
+      "status": "Success"
+    }), 203
+  else:
+    return jsonify({"error":"Personaje no encontrado"}),404
+
+
+
+#Agrego Episodios y locacion
+"""@app.route('/addepisodeslocation', methods=["POST"])
+def addepisodeandlocation():
+ get_from_body = request.json.get("id_character")
+ episodeandlocation = CharacterEpisodeAndLocation() 
+ episodiosylocaciones = CharacterEpisodeAndLocation.query.filter_by(character_id=get_from_body).first()
+ if episodiosylocaciones is not None:
+    return "The character already exist"
+ else:
+    character.character_id = request.json.get("character_id")
+    character.name = request.json.get("name")
+    character.status = request.json.get("status")
+    character.species= request.json.get("species")
+    character.gender =request.json.get("gender")
+
+    db.session.add(character)
+    db.session.commit()  
+
+  return f"Se creo el usuario", 201"""
+
+# Muestro episodios y locacion por id
+@app.route('/characterepisodeandlocation/<int:id>', methods=["GET"])
+def episode_location(id):
+    characterepiandloc: CharacterEpisodeAndLocation.query.filter_by(id=id).first()
+    if characterepiandloc is not None:
+         return jsonify(characterepiandloc.serialize_3()), 200
+    else:
+         return jsonify({"error":"Character not found"}),404
+    
+
+
+
+#Listar los favoritos de un usuario
+@app.route('/favoritecharacter', methods=["GET"])
+def favorite_character():
+    favoritecharacter: Favoritecharacter.query.all()
+    favoritecharacter: list(map(lambda favoritecharacter: favorite_character.serialize_3(), favorite_character))
+   
+    return jsonify({
+    "data": favoritecharacter,
+    "status": 'success'
+  }),200            
+     
+#Anadir un nuevo personaje
+
+   
+   
+#@app.route('/character', methods=['GET'])
+#def handle_hello():
+
+   #response_body = {
+        #"msg": "Hello, this is your GET /character response "}
+   
+   #return jsonify(response_body), 200
 
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+    app.run(host='0.0.0.0', port=PORT, debug=True)
--- src/models.py
+++ src/models.py
@@ -3,17 +3,74 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__= "user"
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    name = db.Column(db.String(250), nullable=False)
+    last_name = db.Column(db.String(250), nullable=False)
+    email = db.Column(db.String(250), unique= True)
+    password = db.Column(db.String(250),unique=False, nullable=False)
+    subscription_date = db.Column(db.Integer, nullable=False)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
 
-    def serialize(self):
+    def serialize_1(self):
+        return {
+        "id": self.id,
+        "name": self.name,
+        "last_name": self.last_name,
+        "email": self.email,
+        "subscription_date": self.subscription_date
+    }
+    
+
+class Character(db.Model):
+    __tablename__ = 'character'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.db.String(250), unique=True)
+    status = db.Column(db.String(250), nullable=False)
+    species = db.Column(db.String(250), nullable=False)
+    gender = db.Column(db.String(250), nullable=False)    
+    
+    def serialize_2(self):
         return {
             "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.name,
+            "status": self.status,
+            "species": self.species,
+            "gender": self.gender  
+            }
+
+class CharacterEpisodeAndLocation(db.Model):
+     __tablename__ = 'characterepisodeandlocation'
+     id = db.Column (db.Integer, primary_key= True)
+     id_character = db.Column (db.Integer, db.ForeignKey('character.id'))
+     episode= db.Column(db.String, nullable= False)
+     location= db.Column(db.String, nullable=False)
+     character= db.relationship(Character)
+     
+     def serialize_3(self):
+        return {
+            "id": self.id,
+            "id_character": self.id_character,
+            "episode": self.episode,
+            "location": self.location
+            }
+
+class Favoritecharacter(db.Model):   
+   __tablename__ = 'favoritecharacter'
+   id = db.Column(db.Integer, primary_key=True)
+   id_user =db.Column(db.Integer, db.ForeignKey('user.id')) # Marca el id del usuario que selecciona un personaje como favorito
+   user = db.relationship(User)
+   id_character = db.Column(db.Integer,db.ForeignKey('character.id')) # Marca el id del personaje que ha sido seleccionado como favorito
+   character = db.relationship(Character)
+
+def serialize_3(self):
+        return {
+
+            "id": self.id,
+            "id_user": self.id_user,
+            "id_ character": self.id_character
         }
+  
+    
+    
+    
